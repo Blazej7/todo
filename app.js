473450e7947cf6ffbb12f9d1951a1cc371ecb4e0
blazej7 <blazej.kowalczyk7@gmail.com>
2019-11-11 12:28:24 +0100

    making router

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import db from './db/db';
+import bodyParser from 'body-parser';
+
+// Set up the express app
+const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
